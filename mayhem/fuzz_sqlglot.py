52ec4f4dfc87065006eec393febc087151d0066c
Vanya <user@orp.local>
2022-12-30 21:24:46 -0500

    added harness

---
+++ mayhem/fuzz_sqlglot.py
@@ -0,0 +1,33 @@
+#! /usr/bin/python3
+
+import atheris
+import sys
+
+with atheris.instrument_imports():
+    import sqlglot
+    from sqlglot import parse_one, exp
+    from sqlglot.optimizer import optimize
+
+
+@atheris.instrument_func
+def test_input(input_bytes):
+    fdp = atheris.FuzzedDataProvider(input_bytes)
+    input_string = fdp.ConsumeUnicodeNoSurrogates(sys.maxsize)
+    input_str_w_surrogates = fdp.ConsumeUnicode(sys.maxsize)
+    input_int = fdp.ConsumeInt(sys.maxsize)
+    
+    try:
+        sqlglot.transpile(input_string, read=input_string, write=input_string)
+        parse_one(input_string).find_all(exp.Column)
+        optimize(sqlglot.parse_one(input_string), 
+            schema={input_string: {input_string: input_string}}
+            ).sql(pretty=True)
+    except AttributeError: # NoneType.something()
+        pass
+
+def main():
+    atheris.Setup(sys.argv, test_input)
+    atheris.Fuzz()
+
+if __name__ == "__main__":
+    main()
